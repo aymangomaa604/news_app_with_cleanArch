c432f310d4bea82ff6d062dc3eba2b37fefac626
aymangomaa604 <aymangomaa604@gmail.com>
2024-08-18 16:40:40 +0300

    create use case

---
+++ lib/features/home/domain/use_case/home_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:news_app_with_cleanarch/core/utils/errors/failure.dart';
+import 'package:news_app_with_cleanarch/features/home/domain/entity/news_entity.dart';
+import 'package:news_app_with_cleanarch/features/home/domain/repo/home_repo.dart';
+
+abstract class UseCase<type, param> {
+  Future<Either<Failure, type>> fetchAllNews();
+}
+
+class FetchAllNewsUseCase extends UseCase<List<NewsEntity>, int> {
+  final HomeRepo homeRepo;
+
+  FetchAllNewsUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<NewsEntity>>> fetchAllNews() async {
+    return await homeRepo.fetchAllNews();
+  }
+}
