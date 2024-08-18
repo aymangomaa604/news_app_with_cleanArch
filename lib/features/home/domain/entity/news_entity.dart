76266529ec958cdfdc59306fc1bb709f2f68bce3
aymangomaa604 <aymangomaa604@gmail.com>
2024-08-18 16:29:14 +0300

    add News Entity

---
+++ lib/features/home/domain/entity/news_entity.dart
@@ -0,0 +1,9 @@
+class NewsEntity {
+  final String image;
+  final String title;
+  final String subTitle;
+  final String url;
+
+  NewsEntity({required this.image, required this.title, required this.subTitle, required this.url});
+
+}
